a483b9fa1af28a6b3272413072f11ec5d1173b70
Vitalia Kokhanova <vitalia.kokhanova@gmail.com>
2024-08-21 17:36:55 +0300

    Переделала задание №2 по комментариям Учёного Кота

--- star_task_week18/assets/script/main.js
+++ star_task_week18/assets/script/main.js
@@ -82,8 +82,21 @@ function showWrapper() {
 		mainWrapper.innerHTML = localStorage.getItem("data");
 	}
 }
-
 showWrapper();
+
+// Функция для выставления рейтинга
+function setRating(event) {
+	const selectedStar = event.currentTarget;
+	let rating = selectedStar.getAttribute("data-rating");
+	stars.forEach((star) => {
+		if (star.getAttribute("data-rating") <= rating) {
+			star.classList.add("selected");
+		} else {
+			star.classList.remove("selected");
+		}
+	});
+	saveData();
+}
 
 // Функции для самих кликов по звёздочкам и изменения рейтинга
 const rateBoxes = document.querySelectorAll(".rating");
@@ -93,18 +106,6 @@ rateBoxes.forEach(function (rateBox) {
 		const stars = rateBox.querySelectorAll(".star");
 		if (e.target.className === "star") {
 			stars.forEach((star) => star.addEventListener("click", setRating));
-			function setRating(event) {
-				const selectedStar = event.currentTarget;
-				let rating = selectedStar.getAttribute("data-rating");
-				stars.forEach((star) => {
-					if (star.getAttribute("data-rating") <= rating) {
-						star.classList.add("selected");
-					} else {
-						star.classList.remove("selected");
-					}
-				});
-				saveData();
-			}
 		} else if (e.target.className === "rating") {
 			stars.forEach((star) => {
 				star.classList.remove("selected");
@@ -114,7 +115,3 @@ rateBoxes.forEach(function (rateBox) {
 		saveData();
 	});
 });
-
-// Почему-то звёзды меняют цвет на желтый только после двойного клика по ним. Наверно это из-за "e.target.className === "star"" - наверно проблема с обработчиков события для каждого "rateBox"?
-
-// Вторая проблема - это то, что после того, как я обновляю страницу, рейтинг сохраняется, но - если я хочу снова поменять выставленную оценку, то просто так это не сделать - нужно сначала нажать на пустое пространство рядом со звёздами (внутри этого div), и только потом можно установить новое значение звёздочек. И тоже по-моему только после двойного клика, а не с первого... В общем я до этого возилась долго, пробовала другие варианты, но там вообще нельзя было поменять значение рейтинга после обновления страницы, а в этом варианте кода хотя бы какими-то методами, но поменять можно. Вообще я поняла, что видимо это было уже излешне - добавлять функцию "очищения" рейтинга, просто мне не хотелось оставлять рейтинг на 1 звезде, если например я совсем не хочу оставлять оценку, если пользователь передумал и оставил без оценки, я пыталась сделать это через classList.toggle('selected'), вместо classList.add('selected'), но там поведение было совсем хаотичным, поэтому придумала пока что только так - что можно нажать на пустое пространство совсем рядышком со звёздами и они очистятся. В общем мне кажется, что есть какой-то способ попроще вообще это всё сделать, но я уже совсем не понимаю как( буду очень рада, если вы сможете помочь:)
--- task2_week18/assets/script/script.js
+++ task2_week18/assets/script/script.js
@@ -1,30 +1,112 @@
 // Переменные + действия для начального состояния сайта
-const taskListWrapper = document.querySelector(".task-list__inner-wrapper");
 const noTasksTitle = document.createElement("p");
-noTasksTitle.classList.add("no-tasks__title");
-taskListWrapper.append(noTasksTitle);
-noTasksTitle.textContent = "Нет задач";
-const clearButton = document.getElementById("clearButton");
-clearButton.disabled = true;
-const addButton = document.getElementById("addButton");
-const input = document.querySelector(".input");
 const taskList = document.createElement("ul");
-taskList.classList.add("task-list");
-taskListWrapper.append(taskList);
-taskList.style.display = "none";
+
+const UI_ELEMENTS = {
+	TASK_LIST_WRAPPER: document.querySelector(".task-list__inner-wrapper"),
+	CLEAR_BUTTON: document.getElementById("clearButton"),
+	ADD_BUTTON: document.getElementById("addButton"),
+	INPUT: document.querySelector(".input"),
+	NO_TASKS_TITLE: noTasksTitle,
+	TASK_LIST: taskList,
+};
+
+UI_ELEMENTS.NO_TASKS_TITLE.classList.add("no-tasks__title");
+UI_ELEMENTS.NO_TASKS_TITLE.textContent = "Нет задач";
+UI_ELEMENTS.TASK_LIST.classList.add("task-list");
+
+UI_ELEMENTS.TASK_LIST_WRAPPER.append(UI_ELEMENTS.NO_TASKS_TITLE);
+UI_ELEMENTS.TASK_LIST_WRAPPER.append(UI_ELEMENTS.TASK_LIST);
+
+// Пустой массив, куда будут падать задачи и их статус выполнения
+let tasksArray = [];
+
+// Функции, которые задают исходное состояние страницы, когда нет никаких задач
+showNoTasksTitle();
+disableClearAllButton();
+
+// Проверяем, есть ли в локальном хранилище данные, если есть, то отрисовываем список задач
+if (localStorage.getItem("tasksArray")) {
+	hideNoTasksTitle();
+	enableClearAllButton();
+	tasksArray = JSON.parse(localStorage.getItem("tasksArray"));
+	renderTasks(tasksArray);
+}
 
 // Функция для клика по кнопке "Добавить"
-function clickAddButton() {
-	if (input.value.trim() === "") {
-		return;
-	} else {
-		taskList.style.display = "block";
-		noTasksTitle.style.display = "none";
+function addTask() {
+	if (!UI_ELEMENTS.INPUT.value.trim()) return;
+	const task = {
+		name: UI_ELEMENTS.INPUT.value,
+		isDone: false,
+	};
+	tasksArray.push(task);
+	hideNoTasksTitle();
+	renderTasks(tasksArray);
+	clearInput();
+	enableClearAllButton();
+	saveData();
+}
+
+UI_ELEMENTS.ADD_BUTTON.addEventListener("click", addTask);
+
+// Функция для клика по кнопке "Очистить список задач"
+function clearTaskList() {
+	showNoTasksTitle();
+	disableClearAllButton();
+	document
+		.querySelectorAll(".task-list__item-box")
+		.forEach((item) => item.remove());
+
+	clearArrayAndLocalStorage();
+}
+
+UI_ELEMENTS.CLEAR_BUTTON.addEventListener("click", clearTaskList);
+
+// Слушатель события клика по кастомизированному чекбоксу
+// Здесь мы меняем и визуальный вид галочки и меняем статус выполнения задачи внутри массива объектов tasksArray
+
+UI_ELEMENTS.TASK_LIST.addEventListener("click", (e) => {
+	// Здесь у нас тег <span>, потому что я кастомизировала чекбокс, и клик происходит по этому тегу. А тег <input> сделан так, что его не видно, по нему нельзя нажать.
+	// Вообще HTML-структура одной задачи выглядит так:
+	/* <div class="task-list__item-box">
+			<li class="task-list__item-name"></li>
+			<label class="label">
+				<input type="checkbox">
+				<span class="unchecked"></span>
+			</label>
+	</div>
+	*/
+
+	if (e.target.tagName === "SPAN") {
+		e.target.classList.toggle("checked");
+		const parentDiv = e.target.closest("div");
+		const childListItem = parentDiv.querySelector("li");
+		childListItem.classList.toggle("done-task");
+
+		if (childListItem.classList.contains("done-task")) {
+			tasksArray.find(
+				(task) => task.name === childListItem.textContent
+			).isDone = true;
+		} else {
+			tasksArray.find(
+				(task) => task.name === childListItem.textContent
+			).isDone = false;
+		}
+		saveData();
+	}
+});
+
+// Функция для рендеринга списка задач
+function renderTasks(array) {
+	UI_ELEMENTS.TASK_LIST.innerHTML = "";
+	array.forEach((task) => {
 		const taskListItem = document.createElement("div");
 		taskListItem.classList.add("task-list__item-box");
-		taskList.append(taskListItem);
+		UI_ELEMENTS.TASK_LIST.append(taskListItem);
 		const taskListItemName = document.createElement("li");
 		taskListItemName.classList.add("task-list__item-name");
+		taskListItemName.textContent = task.name;
 		const taskListItemCheckboxLabel = document.createElement("label");
 		taskListItem.append(taskListItemName, taskListItemCheckboxLabel);
 		const taskListItemCheckbox = document.createElement("input");
@@ -35,54 +117,47 @@ function clickAddButton() {
 			taskListItemCheckbox,
 			taskListItemCheckboxSpan
 		);
-		taskListItemName.textContent = input.value;
-		input.value = "";
-		clearButton.disabled = false;
-	}
-
-	saveData();
+		if (task.isDone === true) {
+			taskListItemCheckboxSpan.classList.add("checked");
+			taskListItemName.classList.add("done-task");
+		}
+	});
 }
 
-addButton.addEventListener("click", clickAddButton);
-
-// Функция для клика по кнопке "Очистить список задач"
-function clearTaskList() {
-	taskList.style.display = "none";
-	noTasksTitle.style.display = "block";
-	clearButton.disabled = true;
-	document
-		.querySelectorAll(".task-list__item-box")
-		.forEach((item) => item.remove());
-	saveData();
+// Функция, чтобы спрятать надпись "Нет задач" и показать сам список
+function hideNoTasksTitle() {
+	UI_ELEMENTS.TASK_LIST.style.display = "block";
+	UI_ELEMENTS.NO_TASKS_TITLE.style.display = "none";
 }
 
-clearButton.addEventListener("click", clearTaskList);
+// Функция, чтобы скрыть список задач и показать надпись "Нет задач"
+function showNoTasksTitle() {
+	UI_ELEMENTS.TASK_LIST.style.display = "none";
+	UI_ELEMENTS.NO_TASKS_TITLE.style.display = "block";
+}
 
-// Сохраняем состояние чекбокса (правда только визуальное)
-taskList.addEventListener("click", (e) => {
-	if (e.target.tagName === "SPAN") {
-		e.target.classList.toggle("checked");
-		const parentDiv = e.target.closest("div");
-		const childListItem = parentDiv.querySelector("li");
-		childListItem.classList.toggle("done-task");
-		saveData();
-	}
-});
+// Функция для очистки инпута
+function clearInput() {
+	UI_ELEMENTS.INPUT.value = "";
+}
+
+// Функция для включения кнопки "Очистить список задач"
+function enableClearAllButton() {
+	UI_ELEMENTS.CLEAR_BUTTON.disabled = false;
+}
+
+// Функция для полного отключения кнопки "Очистить список задач"
+function disableClearAllButton() {
+	UI_ELEMENTS.CLEAR_BUTTON.disabled = true;
+}
+
+// Функция для очистки самого массива от всех задач и локального хранилища от такого объекта
+function clearArrayAndLocalStorage() {
+	tasksArray.length = 0;
+	localStorage.removeItem("tasksArray");
+}
 
 // Фунцкия для сохранения структуры списка в Local Storage
 function saveData() {
-	localStorage.setItem("tasklist", taskList.innerHTML);
+	localStorage.setItem("tasksArray", JSON.stringify(tasksArray));
 }
-
-// Функция для отображения списка после обновления/закрытия сайта
-function showList() {
-	const someData = localStorage.getItem("tasklist");
-	if (someData) {
-		taskList.style.display = "block";
-		noTasksTitle.style.display = "none";
-		clearButton.disabled = false;
-	}
-	taskList.innerHTML = localStorage.getItem("tasklist");
-}
-
-showList();
